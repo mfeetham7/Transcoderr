8f40d5ead5ef20fdfcd8543d36db89012561f91e
Matt Feetham <mfeetham7@gmail.com>
2023-03-22 23:33:11 -0400

    Changed to ffmpeg for faster scanning speeds

--- Transcoderr.py
+++ Transcoderr.py
@@ -1,5 +1,4 @@
 import argparse
-import configparser
 import json
 import os
 import platform
@@ -9,9 +8,7 @@ import sys
 
 os_type = platform.system()
 default_handbrake_exe = 'HandBrakeCLI.exe' if os_type == 'Windows' else 'HandBrakeCLI' if os_type == 'Linux' else sys.exit("Error: Unsupported operating system.")
-default_mediainfo_exe = 'mediainfo'
-config = configparser.ConfigParser()
-config.read('config.ini')
+get_bitrate_executable = 'ffmpeg.exe' if os_type == 'Windows' else 'ffmpeg' if os_type == 'Linux' else sys.exit("Error: Unsupported operating system.")
 traversed_directories = set()
 transcode_queue = set()
 transcoded_files = set()
@@ -22,36 +19,45 @@ def log_error(file, message):
     with open('brokenfiles.txt', 'a') as f:
         f.write(f'{file}: {message}\n')
 
-def get_bitrate(file, mediainfo_exe):
+def get_duration_ffprobe(file, ffprobe_exe='ffprobe'):
     try:
-        result = subprocess.run([mediainfo_exe, file], capture_output=True, text=True, check=True, encoding='utf-8')
-        if not result.stdout:
-            error_message = f"Error: {mediainfo_exe} is not producing any output for the file {file}. Make sure the mediainfo executable is installed and the file is a valid media file."
-            print(error_message)
-            log_error(file, error_message)
-            return None
-        for line in result.stdout.split('\n'):
-            if line.startswith('Bit rate') or line.startswith('Nominal bit rate') or line.startswith('Overall bit rate'):
-                bitrate_str = line.split(':')[1].strip().replace(' kb/s', '').replace(' Mb/s', '').replace(' ', '.')
-                if bitrate_str == 'Constant' or bitrate_str == 'Variable':
-                    return None
-                bitrate = float(bitrate_str)
-                if 'Mb' in line:
-                    bitrate *= 1000000
-                elif 'kb' in line:
-                    bitrate *= 1000
-                return int(bitrate)
-        warning_message = f"Warning: {mediainfo_exe} is not producing any bitrate information for the file {file}. Make sure the mediainfo executable is installed and the file is a valid media file."
-        print(warning_message)
-        log_error(file, warning_message)
-        bitrate = 0
-        return bitrate
+        result = subprocess.run([ffprobe_exe, '-v', 'quiet', '-print_format', 'json', '-show_streams', '-show_format', file], capture_output=True, text=True, encoding='utf-8')
+        media_info = json.loads(result.stdout)
+        duration_seconds = float(media_info['format']['duration'])
+        duration_hours, remainder = divmod(duration_seconds, 3600)
+        duration_minutes, duration_seconds = divmod(remainder, 60)
+        return duration_hours, duration_minutes, duration_seconds
+    except Exception as e:
+        print(f"Error getting duration for file {file}: {e}")
+        return None
+    
+def get_bitrate(file):
+    global get_bitrate_executable
+    try:
+        result = subprocess.run([get_bitrate_executable, '-i', file, '-hide_banner', '-vn'], capture_output=True, text=True, encoding='utf-8', errors='replace', check=True)
+        result = result.stderr
     except subprocess.CalledProcessError as e:
-        error_message = f"Error: {mediainfo_exe} is not producing any output for the file {file}. Make sure the mediainfo executable is installed and the file is a valid media file."
+        result = e.stderr
+    if not result:
+        error_message = f"Error: {get_bitrate_executable} is not producing any output for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file."
         print(error_message)
         log_error(file, error_message)
-        bitrate = 0
-        return bitrate
+        return None
+    bitrate_regex = re.compile(r'bitrate: (\d+(\.\d+)?) (k|M)b/s')
+    match = bitrate_regex.search(result)
+    if match:
+        bitrate = float(match.group(1))
+        unit = match.group(3)
+        if unit == 'M':
+            bitrate *= 1000000
+        elif unit == 'k':
+            bitrate *= 1000
+        return int(bitrate)
+    else:
+        warning_message = f"Warning: {get_bitrate_executable} is not producing any bitrate information for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file."
+        print(warning_message)
+        log_error(file, warning_message)
+        return None
 
 def get_output_file(input_file, export_path):
     input_dir = os.path.dirname(input_file)
@@ -59,10 +65,26 @@ def get_output_file(input_file, export_path):
     output_dir = os.path.join(export_path, os.path.relpath(input_dir, args.import_path).replace('\\', os.path.sep))
     output_filename = f"{input_filename}.mkv"
     output_file = os.path.join(output_dir, output_filename)
-    return output_file
+    if os.path.exists(output_file):
+        print(f"{output_file} already exists. Do you want to overwrite that? (y/n/skip)")
+        c = input().lower()
+        if c == 'overwrite':
+            return output_file
+        elif c == 'n' or c == 'no':
+            file_root, file_ext = os.path.splitext(input_file)
+            output_file = f"{file_root}_transcoded{file_ext}"
+            return output_file
+        elif c == 'skip' or c == 's':
+            return 'null'
+        else:
+            return get_output_file(input_file, export_path)
+    else: 
+        return output_file
 
 def transcode(input_file, export_path, handbrake_exe, target_bitrate=None, preset_file=None):
     output_file = get_output_file(input_file, export_path)
+    if output_file == 'null':
+        return
     output_dir = os.path.dirname(output_file)
     global transcode_number
     global transcoded_files
@@ -120,7 +142,7 @@ def transcode(input_file, export_path, handbrake_exe, target_bitrate=None, prese
     transcoded_files.add(input_file)
     transcode_queue.remove(input_file)
 
-def traverse(root_dir, filter_bitrate, target_bitrate, export_path, handbrake_exe, mediainfo_exe, subfolder_regex, depth=0):
+def traverse(root_dir, filter_bitrate, target_bitrate, export_path, handbrake_exe, subfolder_regex, depth=0):
     global traversed_directories
     global transcode_queue
     supported_extensions = [".mp4", ".mkv", ".avi", ".mpg", ".ts", ".mxf", ".gxf", ".lxf", ".wmv", ".flv", ".mov", ".mp3"]
@@ -141,19 +163,20 @@ def traverse(root_dir, filter_bitrate, target_bitrate, export_path, handbrake_ex
         item_path = os.path.join(full_path, item)
         if os.path.isdir(item_path):
             if depth > 0 or subfolder_regex is None or re.match(subfolder_regex, item):
-                yield from traverse(item_path, filter_bitrate, target_bitrate, export_path, handbrake_exe, mediainfo_exe, subfolder_regex, depth=depth+1)
+                yield from traverse(item_path, filter_bitrate, target_bitrate, export_path, handbrake_exe, subfolder_regex, depth=depth+1)
         elif os.path.isfile(item_path):
             ext = os.path.splitext(item)[1]
             if ext.lower() not in supported_extensions:
                 continue
-            bitrate = get_bitrate(item_path, mediainfo_exe)
+            bitrate = get_bitrate(item_path)
             if bitrate is not None and bitrate > filter_bitrate * 1000000:
                 size = os.path.getsize(item_path) / (1024 * 1024)
-                duration = subprocess.check_output([mediainfo_exe, '--Inform=General;%Duration%', item_path], encoding='utf-8')
-                duration_seconds = int(duration.strip()) / 1000
-                duration_minutes, duration_seconds = divmod(duration_seconds, 60)
-                duration_hours, duration_minutes = divmod(duration_minutes, 60)
-                print(f"{item} ({duration_hours:.0f}h {duration_minutes:.0f}m {duration_seconds:.0f}s, {size:.2f}MB, {bitrate/1000000:.2f} Mbps)", end='')
+                duration_hours, duration_minutes, duration_seconds = get_duration_ffprobe(item_path)
+                if len(item) > 65 + 5:
+                    itemname = item[:65] + '...' + item[-5:]
+                else:
+                    itemname = item
+                print(f"Found: {itemname} ({duration_hours:.0f}h {duration_minutes:.0f}m {duration_seconds:.0f}s, {size:.2f}MB, {bitrate/1000000:.2f} Mbps)\n", end='')
                 transcode_queue.add(item_path)
                 yield (item_path, bitrate)
     traversed_directories.add(full_path)
@@ -179,13 +202,18 @@ def handle_keyboard_interrupt():
     global transcoded_files
     try:
         print(f"\nInterrupt detected. What would you like to do?\nTranscode queue contains {len(transcode_queue)+len(transcoded_files)} item(s).\n1. Save progress and exit\n2. Start transcoding now")
-        choice = input("Enter your choice (1 or 2): ")
+        choice = input("Enter your choice (1 or 2):\n(or 'delete' to reset queues) ")
         if choice == "2":
             print("Starting transcoding...")
+            process_transcode_queue(preconfirm = True)
             return
         elif choice == '1':
             save_transcode_queue()
             print()
+        elif choice == 'delete':
+            os.remove("transcode_queue.json")
+            print("Queue has been deleted")
+            sys.exit(0)
         else:
             print("Invalid choice. Please try again.")
             handle_keyboard_interrupt()
@@ -195,18 +223,23 @@ def handle_keyboard_interrupt():
 
 def process_transcode_queue(preconfirm=False):
     global transcode_queue
+    global transcoded_files
     total_bitrate = 0
     if not transcode_queue:
         print('No files were found.')
         return False
     for path in transcode_queue:
-        bitrate = get_bitrate(path, args.mediainfo_exe)
-        print(f"{path} ({bitrate/1000000:.2f} Mbps)")
+        bitrate = get_bitrate(path)
+        itemname = os.path.basename(path)
+        if len(itemname) > 65 + 5:
+            itemname = itemname[:65] + '...' + path[-5:]
+        else:
+            itemname = itemname
+        print(f"{itemname} ({bitrate/1000000:.2f} Mbps)")
         if bitrate is not None:
             total_bitrate += bitrate
     total_files = len(transcode_queue)
     average_bitrate = total_bitrate / total_files
-    
     if preconfirm:
         confirm = 'yes'
     else:
@@ -214,6 +247,14 @@ def process_transcode_queue(preconfirm=False):
         confirm = input().lower()
     if confirm == 'yes' or confirm == 'y':
         print('Starting transcode process')
+        for file in set(transcode_queue):
+            output_file = get_output_file(file, args.export_path)
+            if output_file == 'null':
+                transcode_queue.remove(file)
+            else:
+                transcode(file, output_file, args.handbrake_exe, target_bitrate=args.target_bitrate)
+        print("Transcoding done.")
+        os.remove("transcode_queue.json")
         return True
     elif confirm == 'save for later' or confirm == 's':
         save_transcode_queue()
@@ -234,7 +275,6 @@ def transcode_queue_found():
     with open("transcode_queue.json", "r") as file:
         data = json.load(file)
         transcode_queue = set(data["transcode_queue"])
-        files = [(path, get_bitrate(path, args.mediainfo_exe)) for path in transcode_queue]
         if "traversed_directories" in data:
             traversed_directories = set(data["traversed_directories"])
         else:
@@ -244,30 +284,14 @@ def transcode_queue_found():
         else: 
             transcoded_files = set()
         if transcode_queue:
-            print("Found files in transcode queue:")
-            total_bitrate=0
-            for path in transcode_queue:
-                bitrate = get_bitrate(path, args.mediainfo_exe)
-                print(f"{path} ({bitrate/1000000:.2f} Mbps)")
-                total_bitrate += bitrate
-            total_files = len(files)
-            average_bitrate = total_bitrate / total_files
-            print(f'Found {total_files} file(s) with an average bitrate of {average_bitrate/1000000:.2f} Mbps. Start the transcode queue? ([yes/no/quit/save for later] or [y/n/q/s]) ')
-            confirm = input().lower()
-            if confirm == "yes" or confirm == "y":
-                for file, _ in files:
-                    transcode(file, args.export_path, args.handbrake_exe, target_bitrate=args.target_bitrate)
-                print("Transcoding done.")
-                os.remove("transcode_queue.json")
-            elif confirm == "q" or confirm == "quit":
-                sys.exit(0)
-            elif confirm == 's' or confirm == 'save for later':
-                save_transcode_queue()
-                sys.exit(0)
-            elif confirm == '':
-                transcode_queue_found
+            r = process_transcode_queue()
+            if r == True:
+                print("Transcode Completed Successfully")
+            elif r == False:
+                return
             else:
-                continue_traversal
+                print("uncaught exception")
+                sys.exit(0)
 
 def process_complete():
     total_original_size = 0
@@ -288,37 +312,28 @@ def process_complete():
 
 def continue_traversal():
     global transcode_queue
-    for file in traverse(args.import_path, args.filter_bitrate, args.target_bitrate, args.export_path, args.handbrake_exe, args.mediainfo_exe, args.subfolder_regex):
+    for file in traverse(args.import_path, args.filter_bitrate, args.target_bitrate, args.export_path, args.handbrake_exe, args.subfolder_regex):
         transcode_queue.add(file[0])
     print("Traversal Complete")
     start_transcoding = process_transcode_queue()
     if start_transcoding:
-        for file in transcode_queue:
-            transcode(file, args.export_path, args.handbrake_exe, target_bitrate=args.target_bitrate)
-        print("Transcoding done.")
-        os.remove("transcode_queue.json")
+        return
     else:
         sys.exit(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Transcode videos with a bitrate greater than the specified threshold.', add_help=False)
     parser.add_argument('import_path', type=str, help='The path to the directory to import files from.')
-    parser.add_argument('-f', '--filter-bitrate', type=int, help='The minimum bitrate of the files to transcode in Mbps. (default: 10)')
-    parser.add_argument('-e', '--export-path', type=str, help='The path to the directory to export transcoded files to. (default: current working directory)')
-    parser.add_argument('-t', '--target-bitrate', type=int, help='The target bitrate for the transcoded files in Mbps. (default: 5)')
+    parser.add_argument('-f', '--filter-bitrate', type=int, default=10, help='The minimum bitrate of the files to transcode in Mbps. (default: 10)')
+    parser.add_argument('-e', '--export-path', type=str, default=os.getcwd(), help='The path to the directory to export transcoded files to. (default: current working directory)')
+    parser.add_argument('-t', '--target-bitrate', type=int, default=5, help='The target bitrate for the transcoded files in Mbps. (default: 5)')
     parser.add_argument('-F', '--flatten', action='store_true', help='Transcoded files will be placed in the export directory without creating a subfolder structure.')
-    parser.add_argument('-H', '--handbrake-exe', type=str, help='The path to the HandBrakeCLI executable. (default: "HandBrakeCLI.exe")')
-    parser.add_argument('-m', '--mediainfo-exe', type=str, help='The path to the mediainfo executable. (default: "mediainfo")')
+    parser.add_argument('-H', '--handbrake-exe', type=str, default="HandBrakeCLI.exe", help='The path to the HandBrakeCLI executable. (default: "HandBrakeCLI.exe")')
+    parser.add_argument('-m', '--ffmpeg-exe', type=str, default=get_bitrate_executable, help='The path to the ffmpeg executable.')
     parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
     parser.add_argument('-p', '--preset', type=str, help='The path to the HandBrakeCLI preset file. (overrules other quality arguments)')
     parser.add_argument('-s', '--subfolder-regex', type=str, help='Only process subfolders whose names match this regular expression.')
     args = parser.parse_args()
-    args.filter_bitrate = config.getint('DEFAULT', 'filter_bitrate', fallback=10) if args.filter_bitrate is None else args.filter_bitrate
-    args.target_bitrate = config.getint('DEFAULT', 'target_bitrate', fallback=5) if args.target_bitrate is None else args.target_bitrate
-    args.export_path = config.get('DEFAULT', 'export_path', fallback=os.getcwd()) if args.export_path is None else args.export_path
-    args.handbrake_exe = config.get('DEFAULT', 'handbrake_exe', fallback=default_handbrake_exe) if args.handbrake_exe is None else args.handbrake_exe
-    args.mediainfo_exe = config.get('DEFAULT', 'mediainfo_exe', fallback=default_mediainfo_exe) if args.mediainfo_exe is None else args.mediainfo_exe
-    config['DEFAULT'].update({k: str(v) for k, v in vars(args).items()})
 
     try:
         if os.path.exists("transcode_queue.json"):
