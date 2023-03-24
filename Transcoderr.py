82883c3b11a7badc95bc832ffbffa37f525dc65d
Matt Feetham <mfeetham7@gmail.com>
2023-03-23 21:16:32 -0400

    +colors, automatic recovery from incomplete jobs

--- Transcoderr.py
+++ Transcoderr.py
@@ -5,6 +5,7 @@ import platform
 import re
 import subprocess
 import sys
+import threading
 
 os_type = platform.system()
 default_handbrake_exe = 'HandBrakeCLI.exe' if os_type == 'Windows' else 'HandBrakeCLI' if os_type == 'Linux' else sys.exit("Error: Unsupported operating system.")
@@ -24,14 +25,18 @@ def get_duration_ffprobe(file, ffprobe_exe='ffprobe'):
     try:
         result = subprocess.run([ffprobe_exe, '-v', 'quiet', '-print_format', 'json', '-show_streams', '-show_format', file], capture_output=True, text=True, encoding='utf-8')
         media_info = json.loads(result.stdout)
-        duration_seconds = float(media_info['format']['duration'])
-        duration_hours, remainder = divmod(duration_seconds, 3600)
-        duration_minutes, duration_seconds = divmod(remainder, 60)
-        return duration_hours, duration_minutes, duration_seconds
+        if 'format' in media_info:
+            duration_seconds = float(media_info['format']['duration'])
+            duration_hours, remainder = divmod(duration_seconds, 3600)
+            duration_minutes, duration_seconds = divmod(remainder, 60)
+            return duration_hours, duration_minutes, duration_seconds
+        else:
+            print(f"\033[33mError getting duration for file \033[36m{file}\033[33m: 'format' key not found in ffprobe output\033[0m")
+            return None
     except Exception as e:
-        print(f"Error getting duration for file {file}: {e}")
+        print(f"\033[33mError getting duration for file \033[36m{file}\033[33m: {e}\033[0m")
         return None
-    
+
 def get_bitrate(file):
     global get_bitrate_executable
     try:
@@ -40,8 +45,8 @@ def get_bitrate(file):
     except subprocess.CalledProcessError as e:
         result = e.stderr
     if not result:
-        error_message = f"Error: {get_bitrate_executable} is not producing any output for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file."
-        print(error_message)
+        error_message = f"\033[33mError: {get_bitrate_executable} is not producing any output for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file.\033[0m"
+        print(f"\033[31m{error_message}\033[0m")
         log_error(file, error_message)
         return None
     bitrate_regex = re.compile(r'bitrate: (\d+(\.\d+)?) (k|M)b/s')
@@ -55,38 +60,49 @@ def get_bitrate(file):
             bitrate *= 1000
         return int(bitrate)
     else:
-        warning_message = f"Warning: {get_bitrate_executable} is not producing any bitrate information for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file."
-        print(warning_message)
+        warning_message = f"\033[33mWarning: {get_bitrate_executable} is not producing any bitrate information for the file {file}. Make sure the ffmpeg executable is installed and the file is a valid media file.\033[0m"
+        print(f"\033[33m{warning_message}\033[0m")
         log_error(file, warning_message)
         return None
 
 def get_output_file(input_file, export_path):
+    global transcode_number
+    base_dir = args.import_path
     input_dir = os.path.dirname(input_file)
     input_filename = os.path.splitext(os.path.basename(input_file))[0]
-    output_dir = os.path.join(export_path, os.path.relpath(input_dir, args.import_path).replace('\\', os.path.sep))
+    output_dir = os.path.join(export_path, os.path.relpath(input_dir, base_dir).replace('\\', os.path.sep))
     output_filename = f"{input_filename}.mkv"
     output_file = os.path.join(output_dir, output_filename)
     if os.path.exists(output_file):
-        print(f"{output_file} already exists. Do you want to overwrite that? (y/n/skip)")
-        c = input().lower()
-        if c == 'overwrite':
+        file_size = os.path.getsize(output_file)
+        file_duration = get_duration_ffprobe(output_file)
+        if file_size == 0 or file_duration == 0 or file_size == None or file_duration == None:
+            print(f"\033[33mOutput file \033[36m{output_file}\033[33m is incomplete (size: {file_size}, duration: {file_duration}). Deleting and re-transcoding.\033[0m")
+            os.remove(output_file)
             return output_file
-        elif c == 'n' or c == 'no':
-            file_root, file_ext = os.path.splitext(input_file)
-            output_file = f"{file_root}_transcoded{file_ext}"
-            return output_file
-        elif c == 'skip' or c == 's':
-            return 'null'
         else:
-            return get_output_file(input_file, export_path)
+            print(f"{output_file} already exists. Do you want to overwrite that? [y]es/[n]o/[s]kip")
+            timer = threading.Timer(30, c = 'skip')
+            timer.start()
+            c = input().lower()
+            timer.cancel()
+            if c == 'overwrite' or c == 'y' or c == 'yes':
+                return output_file
+            elif c == 'n' or c == 'no':
+                file_root, file_ext = os.path.splitext(input_file)
+                output_file = f"{file_root}_transcoded{file_ext}"
+                return output_file
+            elif c == 'skip' or c == 's':
+                return 'null'
+            else:
+                return get_output_file(input_file, export_path)
     else: 
         return output_file
 
 def transcode(input_file, export_path, target_bitrate=None):
-    output_file = get_output_file(input_file, export_path)
-    if output_file == 'null':
+    if export_path == 'null':
         return
-    output_dir = os.path.dirname(output_file)
+    output_dir = os.path.dirname(export_path)
     global transcode_number
     global transcoded_files
     global starting_transcode_queue_length
@@ -103,19 +119,17 @@ def transcode(input_file, export_path, target_bitrate=None):
         )
         match = encoding_status_pattern.search(output_line)
         if match:
-            print(f"\rEncoding Task {match.group('task_num')} of {match.group('total_tasks')}."f"Progress: {match.group('percent')}%, ETA: {match.group('eta')}",end='',flush=True,)
-
-    print(f'Transcoding "{input_file}" to "{output_file}"...')
+            print(f"\r\033[32mEncoding Task {match.group('task_num')} of {match.group('total_tasks')}."f"Progress: {match.group('percent')}%, ETA: {match.group('eta')}\033[0m",end='',flush=True,)
+    print(f'Transcoding "\033[36m{input_file}\033[0m" to "\033[36m{export_path}\033[0m"...')
     cmd = [
         default_handbrake_exe,
         '-i', input_file,
-        '-o', output_file,
+        '-o', export_path,
         '--all-audio',
         '--audio-lang-list', 'all',
         '--all-subtitles',
         '--subtitle-lang-list', 'all',
     ]
-
     if preset_file:
         cmd.extend(['--preset-import-file', preset_file])
     else:
@@ -136,11 +150,10 @@ def transcode(input_file, export_path, target_bitrate=None):
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True) as process:
         for line in process.stdout:
             process_output(line)
-
     process.wait()
-    output_size = os.path.getsize(output_file)
+    output_size = os.path.getsize(export_path)
     output_size_mb = int(output_size) / (1024 * 1024)
-    print(f"\nFinished encoding {input_file}. Output size: {output_size_mb:.2f} MB")
+    print(f"\nFinished encoding \033[36m{input_file}\033[0m. Output size: {output_size_mb:.2f} MB")
     transcode_number = transcode_number + 1
     transcoded_files.add(input_file)
     transcode_queue.remove(input_file)
@@ -151,15 +164,15 @@ def traverse(root_dir, filter_bitrate, target_bitrate, export_path, handbrake_ex
     supported_extensions = [".mp4", ".mkv", ".avi", ".mpg", ".ts", ".mxf", ".gxf", ".lxf", ".wmv", ".flv", ".mov", ".mp3"]
     root_dir = os.path.normpath(root_dir)
     if not os.path.isdir(root_dir):
-        print(f"Error: {root_dir} is not a valid directory.")
+        print(f"Error: \033[36m{root_dir}\033[0m is not a valid directory.")
         sys.exit(1)
     if traversed_directories is None:
         traversed_directories = set()
     full_path = os.path.abspath(root_dir)
     if full_path in traversed_directories:
-        print(f"Previously Traversed {root_dir} - Skipping.")
+        print(f"Previously Traversed \033[36m{root_dir}\033[0m - Skipping.")
         return
-    print(f"Traversing {root_dir}...")
+    print(f"Traversing \033[36m{root_dir}\033[0m...")
     for item in os.listdir(full_path):
         if item.startswith('.'):
             continue
@@ -179,7 +192,7 @@ def traverse(root_dir, filter_bitrate, target_bitrate, export_path, handbrake_ex
                     itemname = item[:65] + '...' + item[-5:]
                 else:
                     itemname = item
-                print(f"Found: {itemname} ({duration_hours:.0f}h {duration_minutes:.0f}m {duration_seconds:.0f}s, {size:.2f}MB, {bitrate/1000000:.2f} Mbps)\n", end='')
+                print(f"\033[32mFound: {itemname} ({duration_hours:.0f}h {duration_minutes:.0f}m {duration_seconds:.0f}s, {size:.2f}MB, {bitrate/1000000:.2f} Mbps)\n\033[0m", end='')
                 transcode_queue.add(item_path)
                 yield (item_path, bitrate)
     traversed_directories.add(full_path)
@@ -189,7 +202,7 @@ def save_transcode_queue():
     global transcoded_files
     global transcode_queue
     file_name = 'transcode_queue.json'
-    print("Saving progress...")
+    print("\033[34mSaving progress...\033[0m")
     data = {
         "traversed_directories": list(traversed_directories),
         "transcode_queue": list(transcode_queue),
@@ -204,10 +217,10 @@ def handle_keyboard_interrupt():
     global transcode_queue
     global transcoded_files
     try:
-        print(f"\nInterrupt detected. What would you like to do?\nTranscode queue contains {len(transcode_queue)+len(transcoded_files)} item(s).\n1. Save progress and exit\n2. Start transcoding now")
+        print(f"\033[33m\nInterrupt detected. What would you like to do?\nTranscode queue contains\033[0m {len(transcode_queue)+len(transcoded_files)}\033[33m item(s).\n1. Save progress and exit\n2. Start transcoding now\033[0m")
         choice = input("Enter your choice (1 or 2):\n(or 'delete' to reset queues) ")
         if choice == "2":
-            print("Starting transcoding...")
+            print("\033[34mStarting transcoding...\033[0m")
             process_transcode_queue(preconfirm = True)
             return
         elif choice == '1':
@@ -215,10 +228,10 @@ def handle_keyboard_interrupt():
             print()
         elif choice == 'delete':
             os.remove("transcode_queue.json")
-            print("Queue has been deleted")
+            print("\033[33mQueue has been deleted\033[0m")
             sys.exit(0)
         else:
-            print("Invalid choice. Please try again.")
+            print("\033[31mInvalid choice. Please try again.\033[0m")
             handle_keyboard_interrupt()
     except KeyboardInterrupt:
             save_transcode_queue()
@@ -231,7 +244,7 @@ def process_transcode_queue(preconfirm=False):
     global preset_file
     total_bitrate = 0
     if not transcode_queue:
-        print('No files were found.')
+        print('\033[33mNo files were found.\033[0m')
         return False
     for path in transcode_queue:
         bitrate = get_bitrate(path)
@@ -248,25 +261,26 @@ def process_transcode_queue(preconfirm=False):
     if preconfirm:
         confirm = 'yes'
     else:
-        print(f'Found {total_files} file(s) with an average bitrate of {average_bitrate/1000000:.2f} Mbps. Start the transcode queue? ([yes/no/quit/save for later] or [y/n/q/s]) ')
+        print(f'\033[34mFound {total_files} file(s) with an average bitrate of {average_bitrate/1000000:.2f} Mbps. Start the transcode queue? ([yes/no/quit/save for later] or [y/n/q/s]) \033[0m')
         confirm = input().lower()
     if confirm == 'yes' or confirm == 'y':
-        print('Starting transcode process')
+        print('\033[34mStarting transcode process\033[0m')
         for file in set(transcode_queue):
-            output_file = get_output_file(file, args.export_path)
-            if output_file == 'null':
+            o = get_output_file(file, args.export_path)
+            if o == 'null':
+                print(f"Removing \033[36m{file}\033[0m due to detected corruption.")
                 transcode_queue.remove(file)
             else:
-                transcode(file, output_file, target_bitrate=args.target_bitrate)
-        print("Transcoding done.")
+                transcode(file, o, target_bitrate=args.target_bitrate)
+        print("\033[32mTranscoding done.\033[0m")
         os.remove("transcode_queue.json")
         return True
     elif confirm == 'save for later' or confirm == 's':
         save_transcode_queue()
-        print('Transcode queue saved for later.')
+        print('\033[34mTranscode queue saved for later.\033[0m')
         return False
     elif confirm == 'quit' or confirm == 'q':
-        print('quitting without saving')
+        print('\033[33mquitting without saving\033[0m')
         sys.exit(0)
     elif confirm =='n' or confirm =='no':
         return False
@@ -291,11 +305,11 @@ def transcode_queue_found():
         if transcode_queue:
             r = process_transcode_queue()
             if r == True:
-                print("Transcode Completed Successfully")
+                print("\033[32mTranscode Completed Successfully\033[0m")
             elif r == False:
                 return
             else:
-                print("uncaught exception")
+                print("\033[31muncaught exception\033[0m")
                 sys.exit(0)
 
 def process_complete():
@@ -310,16 +324,16 @@ def process_complete():
             total_output_size += output_size
         size_difference = total_original_size - total_output_size
         percentage_difference = (size_difference / total_original_size) * 100
-        print(f'Done. Total original size: {total_original_size / (1024 * 1024):.2f} MB, total output size: {total_output_size / (1024 * 1024):.2f} MB, size difference: {percentage_difference:.2f}%.')
+        print(f'\033[32mDone. Total original size: {total_original_size / (1024 * 1024):.2f} MB, total output size: {total_output_size / (1024 * 1024):.2f} MB, size difference: {percentage_difference:.2f}%.\033[0m')
     except:
-        print('done')
+        print('\033[32mdone\033[0m')
         sys.exit(0)
 
 def continue_traversal():
     global transcode_queue
     for file in traverse(args.import_path, args.filter_bitrate, args.target_bitrate, args.export_path, args.handbrake_exe, args.subfolder_regex):
         transcode_queue.add(file[0])
-    print("Traversal Complete")
+    print("\033[34mTraversal Complete\033[0m")
     start_transcoding = process_transcode_queue()
     if start_transcoding:
         return
@@ -343,9 +357,9 @@ if __name__ == '__main__':
 
     try:
         if os.path.exists("transcode_queue.json"):
-            print(f"previous run found - restoring...")
+            print(f"\033[34mprevious run found - restoring...\033[0m")
             transcode_queue_found()
-            print("Continuing Traversal...")
+            print("\033[34mContinuing Traversal...\033[0m")
         continue_traversal()
     except KeyboardInterrupt:
         handle_keyboard_interrupt()
